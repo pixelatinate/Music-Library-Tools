df9f2e4a393f5a7581e9855f27c7dec0427ef4cc
Swasti Mishra <smishr11@tesla2.eecs.utk.edu>
2021-08-27 19:51:07 -0400

    Accounting for multiple albums
    Now the code can read multiple albums when they're grouped with an artist, but we've gotten into deep and vectors are limiting us. Katie will fix the argument input, and I will get us help with converting to maps.

--- Files/lib_info.cpp
+++ Files/lib_info.cpp
@@ -8,11 +8,13 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
 int strtoint(string number);
-void Print(vector<vector<string>> album, vector<int> sets);
+void Print(vector<vector<string>> album, vector<int> sets, vector<int> albumx);
 //string TotalTime(vector<vector<string>> album, vector<int> sets);
 // Functions
 
@@ -55,14 +57,19 @@ int main(){
     // I haven't cleaning this section up yet because it needs more work
 
     vector<int> usedlisting;
-    vector<int> matchingset;                                                //throw set of matching into it and then print out
+    vector<int> matchingset;
+    vector<int> matchsetartist ;
+    vector<int> matchsetalbum ;                                                //throw set of matching into it and then print out
     vector<vector<string>> Albumlisting(readin.size());
+    vector<string> temporary ;
     
 
     int used = 0;
     for(int i = 0; i < var-1; i++){                                         //was gonna search through the artists at position 2 to see if they match
         used = 0;     
-        matchingset.erase(matchingset.begin(), matchingset.end());                      
+        matchingset.erase(matchingset.begin(), matchingset.end()); 
+        matchsetalbum.erase(matchsetalbum.begin(), matchsetalbum.end());
+                             
                                                                             //kinda lost on what to do with matching though
                                                                             //looking at this http://www.cplusplus.com/forum/general/45549/
                                                                             //for some inspiration on how to find the matching artists
@@ -70,21 +77,26 @@ int main(){
             if(storage[j].at(2) == storage[i].at(2)){
                 matchingset.push_back(j);
                 usedlisting.push_back(j);
+            }
+            if(storage[j].at(3) == storage[i].at(3)){
+                matchsetalbum.push_back(j);
+
             }                                                               //finds all of the matching ones in the set
         }
         for(int k = 0; k < (usedlisting.size()-matchingset.size()); k++){
             for(int m = 0; m < matchingset.size(); m++){
                 if(matchingset.at(m) == usedlisting.at(k)){
-                    //cout << "found same" << endl;
                     used = 1;
                 }                                   
             }
         }
+        
         if(used==1){
             continue;
         }
+        
         else{
-            Print(storage, matchingset);
+            Print(storage, matchingset, matchsetalbum);
         }
         //TotalTime(storage, matchingset);
         //Print(storage, matchingset);
@@ -96,7 +108,10 @@ int main(){
         //used =+ matchingset.size();
         //cout << used;
         //storage.shrink_to_fit();
+        
         matchingset.erase(matchingset.begin(), matchingset.end());
+        matchsetalbum.erase(matchsetalbum.begin(), matchsetalbum.end());
+        
     }
     return 0;
 }
@@ -104,15 +119,30 @@ int main(){
 // End of section I haven't cleaned up yet, the print below is clean
 
 
-void Print(vector<vector<string>> album, vector<int> sets){
+void Print(vector<vector<string>> album, vector<int> sets, vector<int> albumx){
     char space = ' ' ;
-    printf("%s: %d, total time\n %8c%s: %d, total time\n",
-    album[sets.at(0)].at(2).c_str(), sets.size(), space, album[sets.at(0)].at(3).c_str(), sets.size());
+    printf("%s: %d, total time\n",
+    album[sets.at(0)].at(2).c_str(), sets.size()) ;
+    
+    for ( int j = 0 ; j < albumx.size()-1 ; j++){
+        if ( album[sets.at(j)].at(3) == album[sets.at(j+1) ].at(3) ){
+            continue ;
+        }
+        else{
+            printf("%8c%s: %d, total time\n" , space, album[sets.at(j+1)].at(3).c_str(), sets.size());  
+            printf("%16c%s. %s: %s\n", space, album[sets.at(j+1)].at(5).c_str(),
+                    album[sets.at(j+1)].at(0).c_str(), album[sets.at(j+1)].at(1).c_str());  
+        }
+
+    }
+    printf("%8c%s: %d, total time\n" , space, album[sets.at(0)].at(3).c_str(), sets.size());
+
     // * we need to make a function that finds the total amount of time in the file so that can be impemented into the code
     
     for(int i = 0; i < sets.size(); i++){
         printf("%16c%s. %s: %s\n", space, album[sets.at(i)].at(5).c_str(),
         album[sets.at(i)].at(0).c_str(), album[sets.at(i)].at(1).c_str());
+        
     }
 
 }
